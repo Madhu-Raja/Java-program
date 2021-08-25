c8b3a32845c8cc87c737973fef31daf850edf8bd
Madhumitha Raja <89344792+Madhu-Raja@users.noreply.github.com>
2021-08-25 14:04:10 +0530

    Create PerimeterRectangle.java

---
+++ PerimeterRectangle.java
@@ -0,0 +1,15 @@
+public class MyClass { 
+    public  void area(){
+        int s= 10;
+        int t= 5;
+       
+        System.out.print("Perimeter  of rectangle is"+ (s+t));
+    }
+    public static void main(String args[]) {
+        MyClass c= new MyClass();
+        c.area();
+     
+    }
+}
+output
+Perimeter  of rectangle is15
