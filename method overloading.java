7e294bf4300a474a07010bf30b61c833bcdfb556
Madhumitha Raja <89344792+Madhu-Raja@users.noreply.github.com>
2021-09-14 17:10:10 +0530

    Create method overloading.java
    /area.java:8: error: package system does not exist
            system.out.println(a*b);
                  ^
    1 error
    // online compiler

---
+++ method overloading.java
@@ -0,0 +1,19 @@
+public class area{
+    void Area(int s){
+        System.out.println(s*s);
+        
+        
+    }
+    void Area(int a, int b){
+        system.out.println(a*b);
+    }
+
+
+    public static void main(String[]args){
+        area obj=new area();
+        obj. Area(3);
+        obj.Area(2,4);
+        
+    }
+}
+
