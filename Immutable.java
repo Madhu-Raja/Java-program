e0a0f1395156e6491f1f0ae40f610f1b922d86d7
Madhumitha Raja <89344792+Madhu-Raja@users.noreply.github.com>
2021-09-16 17:52:38 +0530

    Create Immutable.java

---
+++ Immutable.java
@@ -0,0 +1,23 @@
+public class Stringimmutable
+{
+    public static void main(String []args){
+        String s="Java strings";
+        s.concat("cheat sheet") ;
+        System.out.println(s);}
+    
+}
+output
+Java strings
+//
+import java.util.String* ;
+public class Stringimmutable
+{
+    public static void main(String []args){
+        String Str="Welcome to Java";
+        Str1 =Str.replace('a','e');
+        System.out.println(Str1);
+        
+    }
+}
+output
+Welcome to Jeve
