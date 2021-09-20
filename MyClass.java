e319f69b023081565e8c04ab2f18d7692853dbfc
Madhumitha Raja <89344792+Madhu-Raja@users.noreply.github.com>
2021-09-20 20:08:52 +0530

    Create MyClass.java

---
+++ MyClass.java
@@ -0,0 +1,24 @@
+// Upper to Lower case
+public class MyClass {
+   public static void main(String []args){
+       String str="WELCOME TO JAVA";
+       String str1= str.toLowerCase();
+       System.out.println(str1);
+      
+   }
+}
+output
+welcome to java
+
+// Lower to Upper case
+
+public class MyClass {
+   public static void main(String []args){
+       String str=" hello world";
+       String str1= str.toUpperCase();
+       System.out.println(str1);
+      
+   }
+}
+ouput
+ HELLO WORLD
