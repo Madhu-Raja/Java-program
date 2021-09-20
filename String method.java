f3535fce7008a59c3843d14a6bc668925983194d
Madhumitha Raja <89344792+Madhu-Raja@users.noreply.github.com>
2021-09-20 20:27:19 +0530

    Create String method.java

---
+++ String method.java
@@ -0,0 +1,25 @@
+// is contains function
+public class MyClass {
+   public static void main(String []args){
+       String str=" Welcome to java";
+       
+       System.out.println(str.contains("a"));
+      
+   }
+} 
+output
+true
+// concating the string
+public class MyClass {
+   public static void main(String []args){
+      String str = "welcome ";
+      String str1 = "to java";
+      System.out.println(str.concat(str1));
+       
+       
+      
+   }
+}
+output
+welcome to java
+
