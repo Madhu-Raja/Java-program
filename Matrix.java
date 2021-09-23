e75ecdaa2cb9e5b325ee6d3a48a1a1c72130c80c
Madhumitha Raja <89344792+Madhu-Raja@users.noreply.github.com>
2021-09-23 12:13:50 +0530

    Create Matrix.java

---
+++ Matrix.java
@@ -0,0 +1,12 @@
+class Myclass{
+    public static void main(String[]args)
+    {
+        int matrix[][]={{3,2,1},{2,4,3},{4,3,2}};
+       
+        for (int i=0;i<3;i++){
+            for (int j=0;j<3;j++){
+                System.out.println(matrix[j][i]);
+            }
+        }
+    }
+}
