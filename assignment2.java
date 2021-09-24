82caa0f9df69c8f68fe47258744cffa42c3f1d75
Madhumitha Raja <89344792+Madhu-Raja@users.noreply.github.com>
2021-09-24 20:39:36 +0530

    Update assignment2.java

--- assignment2.java
+++ assignment2.java
@@ -1,4 +1,5 @@
-// assignment 2 set 1
+// Assignment 2 set 1
+// Addition of floating points using inheritance
 class one{
     float s=7.5f;
     
@@ -14,6 +15,44 @@ public class two extends one{
 }
 output
 9.8
+// Addition of floating numbers using abstract class
+abstract class Sum{  
+	  abstract void add();  
+	}  
+	  	 public class MyClass extends Sum{  
+	void add()
+      {
+          float s=33.4f,d=23.5f;
+          System.out.println(s+d);
+          
+      }  
+	  
+	public static void main(String args[]){  
+	 Sum obj = new MyClass();  
+	 obj.add();  
+	 }  
+}  
 
+ouput
+56.9
 
+// Adiition of floating numbers using interface
+interface Sum{  
+	void add();  
+	}  
+	  
+	public class MyClass implements Sum{  
+	public void add(){
+	    float s=32.43f, t=22.3f;
+	    System.out.println(s+t);
+	    
+	}  
+	  
+	public static void main(String args[]){  
+	MyClass obj = new MyClass();  
+	obj.add();  
+	 }  
+	}  
+output
+54.73
 
