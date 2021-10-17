4df207b8d851963d6c0a453bdcd4986889f07331
Madhumitha Raja <89344792+Madhu-Raja@users.noreply.github.com>
2021-10-17 14:27:49 +0530

    Create Lefttriangle.java

---
+++ Lefttriangle.java
@@ -0,0 +1,26 @@
+public class MyClass 
+{
+public static void main(String[]args)
+{
+int i,j,r=6;
+for(i=0;i<r;i++)
+{
+for(j=2*(r-i);j>=0;j--)
+{
+System.out.print(" ");
+}
+for (j=0;j<=i;j++)
+{
+System.out.print("* ");
+}
+System.out.println();
+}
+}
+}
+output
+             * 
+           * * 
+         * * * 
+       * * * *              
+     * * * * * 
+   * * * * * * 
