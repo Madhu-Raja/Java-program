60cb21aec8a4f7b78bb83db22f64f6ad62c9d107
Madhumitha Raja <89344792+Madhu-Raja@users.noreply.github.com>
2022-08-26 20:09:45 +0530

    Create ROperatorExample.java

---
+++ ROperatorExample.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class ROperatorExample {
+
+	public static void main(String[] args) {
+     int a=10;
+     int b=2;
+     int c=4;
+     int d=0,e=0;
+     if(a>b) {
+    	 d=100;
+     }
+     if(a<b) {
+    	 d=34;
+     }
+     if(a!=c) {
+    	 e=100;
+     }
+     if(a==c) {
+    	 e=5;
+     }
+     if(a<=c) {
+    	 e=5;
+     }
+     System.out.println(a);
+     System.out.println(b);
+     System.out.println(c);
+     System.out.println(d);
+     System.out.println(e);
+	}
+
+}
+output:
+10
+2
+4
+100
+100
